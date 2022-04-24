1b714406b1be7d12bdfde67a030cf7832380b2ca
hossein-ghadirzadeh <hossein.ghadirzadeh@gmail.com>
2022-04-24 15:47:41 +0430

    feat(FileUpload): update app.js file and index.scss

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import FileUpload from "./components/FileUpload";
 
 const App = () => {
-  return <div>Hello World</div>;
+  const [uploadedFilesData, setUploadedFilesData] = useState([]);
+  const handleOnUploadFiles = (files) => {
+    if (uploadedFilesData.length > 0) {
+      const newUploadedFiles = [...uploadedFilesData];
+      Object.keys(files).forEach((fileName) => {
+        newUploadedFiles.push({
+          name: fileName,
+          progress: 100,
+          isFailed: false,
+        });
+      });
+      setUploadedFilesData(newUploadedFiles);
+    } else {
+      const newUploadedFiles = Object.keys(files).map((fileName) => {
+        return {
+          name: fileName,
+          progress: 100,
+          isFailed: false,
+        };
+      });
+      setUploadedFilesData(newUploadedFiles);
+    }
+  };
+  const handleOnRemoveFile = (fileName) => {
+    const uploadedFiles = [...uploadedFilesData];
+    const filteredFiles = uploadedFiles.filter(
+      (file) => file.name !== fileName
+    );
+    setUploadedFilesData(filteredFiles);
+  };
+  return (
+    <div>
+      <FileUpload
+        onUploadFiles={handleOnUploadFiles}
+        uploadedFiles={uploadedFilesData}
+        onRemoveFile={handleOnRemoveFile}
+        error={false}
+      />
+    </div>
+  );
 };
 
 export default App;
